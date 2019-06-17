fecb802d03a162f9157558e2e609c6d4e44941ce
Kleber Andrade <klebersandrade82@gmail.com>
2019-06-16 23:30:15 -0300

    adicionado get all rebeldes

--- src/main/java/com/starwars/resistence/controller/RebeldeController.java
+++ src/main/java/com/starwars/resistence/controller/RebeldeController.java
@@ -3,6 +3,9 @@ package com.starwars.resistence.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,5 +46,8 @@ public class RebeldeController {
 		return rebelde;
 	}
 	
-	
+	@GetMapping("/rebeldes")
+	public Page<Rebelde> getRebeldes(Pageable pageable) {
+		return rebeldeRepository.findAll(pageable);
+	}
 }
