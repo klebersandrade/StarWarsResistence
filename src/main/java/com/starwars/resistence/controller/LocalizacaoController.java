0b8e6a4002916fc90c71f545c632fe96f059c6cd
klebersandrade82 <kleber@synctech.com.br>
2019-06-25 09:45:00 -0300

    Adicionado GET de localizações

--- src/main/java/com/starwars/resistence/controller/LocalizacaoController.java
+++ src/main/java/com/starwars/resistence/controller/LocalizacaoController.java
@@ -1,14 +1,18 @@
 package com.starwars.resistence.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.starwars.resistence.exception.ResourceNotFoundException;
+import com.starwars.resistence.model.Inventario;
 import com.starwars.resistence.model.Localizacao;
 import com.starwars.resistence.repository.LocalizacaoRepository;
 import com.starwars.resistence.repository.RebeldeRepository;
@@ -31,6 +35,11 @@ public class LocalizacaoController {
 			localizacao.setRebelde(rebelde);
 			return localizacaoRepository.save(localizacao);
 		}).orElseThrow(() -> new ResourceNotFoundException("rebeldeId "+ rebeldeId + " não encontrado!"));		
+	}
+	
+	@GetMapping("/rebeldes/localizacoes/{id}")
+	public List<Localizacao> getLocalizacoes(@PathVariable Long id) {
+		return localizacaoRepository.findByRebeldeId(id, null).getContent();
 	}
 
 }
